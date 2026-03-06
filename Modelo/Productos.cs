ac1fd6507ff1b73c81763c82f1284bdda6097392
velacandledev <velacandledev@gmail.com>
2026-03-05 21:20:45 -0600

    Moved cargarProductos() into obtenerProductos() and installed RestSharp and Newtonsoft.Json on "Modelo".

--- Modelo/Productos.cs
+++ Modelo/Productos.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using RestSharp;
+using Newtonsoft.Json;
 
 namespace Modelo
 {
@@ -12,5 +14,17 @@ namespace Modelo
         public string? Descripcion { get; set; }
         public double Precio{ get; set; }
         public DateTime Fecha { get; set; }
+
+        private async void obtenerProductos()
+        {
+            RestClient cliente = new RestClient();
+            RestRequest peticion = new RestRequest("https://67db76a51fd9e43fe4749f9c.mockapi.io/api/v1/Productos", Method.Get);
+            var res = await cliente.ExecuteGetAsync(peticion);
+
+            List<Productos> lista = (List<Productos>)JsonConvert.DeserializeObject(res.Content, typeof(List<Productos>));
+
+            // dgvProductos.DataSource = lista;
+            // listaProductos = lista;
+        }
     }
 }
--- Vista/Form1.cs
+++ Vista/Form1.cs
@@ -33,14 +33,7 @@ namespace Vista
 
         private async void cargarProductos()
         {
-            RestClient cliente = new RestClient();
-            RestRequest peticion = new RestRequest("https://67db76a51fd9e43fe4749f9c.mockapi.io/api/v1/Productos", Method.Get);
-            var res = await cliente.ExecuteGetAsync(peticion);
-
-            List<Productos> lista = (List<Productos>)JsonConvert.DeserializeObject(res.Content, typeof(List<Productos>));
-
-            dgvProductos.DataSource = lista;
-            listaProductos = lista;
+            
         }
 
         private void btnActualizar_Click(object sender, EventArgs e)
